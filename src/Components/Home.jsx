4d1ae54fd4a25cec257911e145fa990f83bbbe7f
Anjila-26 <anjilakshetri@gmail.com>
2023-10-14 11:43:42 +0545

    photo change

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/photo.jpg";
+import image from "../images/anjilaphoto.jpg";
 
 const imageAltText = "Anjila Subedi - AI Enginner and Backend Developer";
 
